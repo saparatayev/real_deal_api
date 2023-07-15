5c918f8b72f501738eaa44f0dc7ea306082e94da
saparatayev <sopa.97@mail.ru>
2023-07-15 16:52:48 +0500

    one-to-one relation, user account_id cannot be null

--- lib/real_deal_api/accounts/account.ex
+++ lib/real_deal_api/accounts/account.ex
@@ -7,6 +7,7 @@ defmodule RealDealApi.Accounts.Account do
   schema "accounts" do
     field :email, :string
     field :hash_password, :string
+    has_one :user, RealDealApi.Users.User
 
     timestamps()
   end
