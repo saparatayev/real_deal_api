f7f44b45157b3830cf1ca35cb177aaf43b29b0d6
saparatayev <sopa.97@mail.ru>
2023-07-20 20:19:30 +0500

    sign_in endpoint, error handling

--- lib/real_deal_api_web/router.ex
+++ lib/real_deal_api_web/router.ex
@@ -1,6 +1,14 @@
 defmodule RealDealApiWeb.Router do
   use RealDealApiWeb, :router
 
+  defp handle_errors(conn, %{reason: %Phoenix.Router.NoRouteError{message: message}}) do
+    conn |> json(%{errors: message}) |> halt()
+  end
+
+  defp handle_errors(conn, %{reason: %{message: message}}) do
+    conn |> json(%{errors: message}) |> halt()
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
@@ -9,5 +17,6 @@ defmodule RealDealApiWeb.Router do
     pipe_through :api
     get "/", DefaultController, :index
     post "/accounts/create", AccountController, :create
+    post "/accounts/sign_in", AccountController, :sign_in
   end
 end
