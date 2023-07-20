cca2b57136dade5c15c44a7187fea00d0b5d7505
saparatayev <sopa.97@mail.ru>
2023-07-20 16:48:39 +0500

    password hashing, secret key, guardian and bcrypt libs

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,10 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :real_deal_api, RealDealApiWeb.Auth.Guardian,
+  issuer: "real_deal_api",
+  secret_key: "2AJMXdd8Kb7oia5vn2lKJ42kdDz6EjQ+s7D5b4vJGbrMUcCZxSmxZHXtGbaY5/gN"
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
