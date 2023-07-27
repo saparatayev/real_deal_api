ddc4537c5372a9bc7448099d60f1eaae592f7a5f
saparatayev <sopa.97@mail.ru>
2023-07-27 12:25:16 +0500

    refactor Account schema

--- lib/real_deal_api/accounts/account.ex
+++ lib/real_deal_api/accounts/account.ex
@@ -2,21 +2,26 @@ defmodule RealDealApi.Accounts.Account do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @optional_fields [:id, :inserted_at, :updated_at]
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "accounts" do
     field :email, :string
-    field :hash_password, :string
+    # field :hash_password, :string
     has_one :user, RealDealApi.Users.User
 
     timestamps()
   end
 
+  defp all_fields() do
+    __MODULE__.__schema__(:fields)
+  end
+
   @doc false
   def changeset(account, attrs) do
     account
-    |> cast(attrs, [:email, :hash_password])
-    |> validate_required([:email, :hash_password])
+    |> cast(attrs, all_fields())
+    |> validate_required(all_fields() -- @optional_fields)
     |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "Must have the @ sign and no spaces")
     |> validate_length(:email, max: 160)
     |> unique_constraint(:email)
